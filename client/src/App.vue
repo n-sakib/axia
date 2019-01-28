f30ac140e025a1c933dad932cc30e24cc72d1c94
n-sakib <ns.sanim@gmail.com>
2019-01-28 16:35:55 -0600

    fixing vue deps

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,6 +7,12 @@
 
 <script>
 import Header from '@/components/Header'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from 'vue'
+
+Vue.use(BootstrapVue)
 
 export default {
   name: 'App',
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -56,3 +56,30 @@ export default {
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.bg-info {
+    background-color: #fff !important;
+}
+.navbar-dark .navbar-nav .nav-link {
+    color: #000;
+}
+.navbar-dark .navbar-nav .nav-link {
+  color: #000;
+}
+</style>
