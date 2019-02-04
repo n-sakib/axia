df123c372dca6babe132361d7dd9df6a0a104285
n-sakib <ns.sanim@gmail.com>
2019-02-04 16:18:13 -0600

    Grid is now established.

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,31 +1,9 @@
 <template>
   <div class="footer">
+    <b-container fluid class="bv-example-row">
     <b-row>
-    <b-col cols="3">
-      <h3 class="customer-support-footer">Customer Support</h3>
-      <ul>
-        <li class="customer-support-list">+X-XXX-XXXX-XX</li>
-        <li class="customer-support-list">Start Chat</li>
-        <li class="customer-support-list">E-mail</li>
-      </ul>
-    </b-col>
-    <b-col cols="3">
-      <h3>ABOUT US</h3>
-      <ul>
-        <li>Our Story</li>
-        <li>Axia Blog</li>
-      </ul>
-    </b-col>
-    <b-col cols="3">
-      <h3>PRODUCT</h3>
-      <ul>
-        <li>Licence</li>
-        <li>Custom Music</li>
-        <li>Music</li>
-      </ul>
-    </b-col>
-    <b-col cols="3">
-      <h3>PLAYLIST</h3>
+    <b-col cols="2">
+      <h3 class="playlist-footer">PLAYLIST</h3>
       <ul>
         <li>What's New</li>
         <li>Cinematic</li>
@@ -33,8 +11,41 @@
         <li>View All</li>
       </ul>
     </b-col>
-
+    <b-col cols="2">
+      <h3 class="product-footer">PRODUCT</h3>
+      <ul>
+        <li>Licence</li>
+        <li>Custom Music</li>
+        <li>Music</li>
+      </ul>
+    </b-col>
+    <b-col cols="2">
+      <h3 class="about-us-footer">ABOUT US</h3>
+      <ul>
+        <li>Our Story</li>
+        <li>Axia Blog</li>
+      </ul>
+    </b-col>
+    <b-col cols="6">
+      <div class="footer-right">
+        /* Right footer content here */
+      </div>
+    </b-col>
   </b-row>
+    </b-container>
+
+<div class="bottom-nav-bar">
+        <ul>
+          <li class="facebook-icon"></li>
+          <li class="twitter-icon"></li>
+          <li class="instagram-icon"></li>
+          <li class="soundcloud-icon"></li>
+          <li class="youtube-icon"></li>
+          <li class="bottom-nav-bar-links">Privacy Policy</li>
+          <li class="bottom-nav-bar-links">Terms of Use</li>
+          <li class="bottom-nav-bar-links">Contact Us</li>
+        </ul>
+    </div>
   </div>
 </template>
 <script>
@@ -60,14 +71,32 @@ h1, h2 {
 h3.customer-support-footer {
   color:#ff6600;
 }
+h3.about-us-footer {
+  position: relative;
+    text-align: left;
+      margin-left: 33px;
+}
+h3.product-footer {
+  position: relative;
+    text-align: left;
+      margin-left: 33px;
+}
+h3.playlist-footer {
+  position: relative;
+  text-align: left;
+  margin-left: 33px;
+}
 ul {
   list-style-type: none;
   padding: 0;
 }
 li {
   display: block;
-  margin: 43px;
   font-size: 12px;
+  text-align: left;
+  position: relative;
+    margin-left: 35px;
+    margin-bottom: 16px;
 }
 li.customer-support-list {
   color: #ff6600;
@@ -80,6 +109,27 @@ a {
   background-color: #000;
   text-align: center;
   color: #9000fa;
+}
+.footer .footer-right {
+  background-color: #8f1717;
+  height: 500px;
+  width: 100%;
+  float: right;
+  position: absolute;
+}
+.bottom-nav-bar {
+  background-color: #42b983;
+  height: 100px;
+  overflow: hidden;
+  position: relative;
+  bottom: 0px;
+  width: 100%;
+}
+li.bottom-nav-bar-links {
+  display: inline-block;
+  position: absolute;
+  left: 50%;
+  font-size: 12px;
 }
 .bg-info {
     background-color: #17a2b8 !important;
